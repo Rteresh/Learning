c38412840aeefba4465bac219f9e25f6a9ab121d
Roman <48884863+Rteresh@users.noreply.github.com>
2021-04-05 17:45:13 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -5,4 +5,7 @@ class Settings:
         self.screen_name = 'Mine 5-1'
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (255, 255, 255)
+
+        # Настройки комбайна
+        self.combine_speed = 1.5
